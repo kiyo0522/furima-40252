c015b6b1f8dd97affc8853f3f7b73cb466ac791a
kiyo0522 <kiyoko.ftis@gmail.com>
2024-06-02 22:43:09 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]  # ログインしていることを確認
+  before_action :contributor_confirmation, only: [:edit, :update] # 現在のユーザーとアイテム投稿者が一致していることを確認
+
   def index
     @items = Item.includes(:user).order(created_at: :desc) # 新着順にソート
   end
@@ -22,14 +24,24 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id]) # 編集したいレコードを取得
+    if @item.update(item_params)# 取得したレコードをupdateメソッドで更新
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # @item.destroy
-    # redirect_to root_path
+    # @item = Item.find(params[:id]) 
+    # if @item.destroy
+    #   redirect_to root_path
+    # else
+    #   redirect_to root_path
   end
 
   private
@@ -46,5 +58,10 @@ class ItemsController < ApplicationController
       :image,
       :price
     ).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 end
